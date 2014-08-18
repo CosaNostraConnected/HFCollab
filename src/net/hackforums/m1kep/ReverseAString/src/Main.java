35f0faf7941846cde7deeac453eee8bf394c66bb
M1kep <M1kep.my.mail@gmail.com>
2014-08-18 16:10:28 -0700

    Updated ReverseAString to use less string variables

--- src/net/hackforums/m1kep/ReverseAString/src/Main.java
+++ src/net/hackforums/m1kep/ReverseAString/src/Main.java
@@ -5,19 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		// Used to read input from console.
 		Scanner input = new Scanner(System.in);
-
-		String reversedString = "";
-
 		System.out.println("Welcome to M1kep's String Reverser!");
-		System.out.println("Enter the string you would like to reverse and then press enter: ");
+		System.out.print("Enter the string you would like to reverse and then press enter: ");
 		// Set string 'plainString' equal to user input.
-		String plainString = input.nextLine();
-		// Convert string into an array of characters.
-		char[] chars = plainString.toCharArray();
+		char[] charArray = input.nextLine().toCharArray();
+		String reversedString = "";
 		// Iterate through the array in reverse and add the letters to the
 		// string 'reversedString'
-		for (int i = chars.length - 1; i >= 0; i--) {
-			reversedString = reversedString + chars[i];
+		for (int i = charArray.length - 1; i >= 0; i--) {
+			reversedString = reversedString + charArray[i];
 		}
 		System.out.println(reversedString);
 
