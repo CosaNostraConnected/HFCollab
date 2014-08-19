94ebf5a599e09675644f828ef3d8a918084374da
M1kep <M1kep.my.mail@gmail.com>
2014-08-18 17:53:44 -0700

    Fixed some formatting

--- src/net/hackforums/m1kep/ReverseAString/src/Main.java
+++ src/net/hackforums/m1kep/ReverseAString/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.util.Scanner;
 
 public class Main {
@@ -5,11 +6,13 @@ public class Main {
 	public static void main(String[] args) {
 		// Used to read input from console.
 		Scanner input = new Scanner(System.in);
+		String reversedString = "";
+
 		System.out.println("Welcome to M1kep's String Reverser!");
 		System.out.print("Enter the string you would like to reverse and then press enter: ");
 		// Set string 'plainString' equal to user input.
 		char[] charArray = input.nextLine().toCharArray();
-		String reversedString = "";
+
 		// Iterate through the array in reverse and add the letters to the
 		// string 'reversedString'
 		for (int i = charArray.length - 1; i >= 0; i--) {
