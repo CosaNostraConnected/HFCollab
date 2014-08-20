ce153b360b33ad6e289f373bb967985019a47064
Psifour <OutdatedConcept@gmail.com>
2014-08-19 22:29:09 -0500

    Moved/added package declaration

---
+++ src/net/hackforums/m1kep/Main.java
@@ -0,0 +1,8 @@
+package testingGit;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
--- src/net/hackforums/thehfgame/Main.java
+++ src/net/hackforums/thehfgame/Main.java
@@ -2,4 +2,7 @@ package net.hackforums.thehfgame;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+    }
 }
