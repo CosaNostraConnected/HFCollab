d3b1c65332ddb201751ecd6a046465abbecdd5d2
Psifour <OutdatedConcept@gmail.com>
2014-08-21 17:53:37 -0500

    Fixed capitalization in package-name

--- src/net/hackforums/m1kep/ExperimentationCorner/Main.java
+++
@@ -1,8 +0,0 @@
-package net.hackforums.m1kep.ExperimentationCorner;
-
-public class Main {
-  public static void Main(String[] args)
-  {
-
-  }
-}
---
+++ src/net/hackforums/m1kep/experimentationcorner/Main.java
@@ -0,0 +1,8 @@
+package net.hackforums.m1kep.experimentationcorner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
