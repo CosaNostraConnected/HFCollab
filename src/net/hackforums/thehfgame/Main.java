859ad7c8052176f759aa79c7bf34d2c94506df68
robanator1617 <robbie.harding@hotmail.com>
2014-08-22 10:26:24 -0400

    Added some basic functions - Lets start this!

--- src/net/hackforums/thehfgame/Main.java
+++ src/net/hackforums/thehfgame/Main.java
@@ -1,8 +1,30 @@
 package net.hackforums.thehfgame;
 
-public class Main {
+public class Main implements Runnable{
+    
+    boolean isRunning = false;
+    private Thread thread;
+    
+    public void start() {
+        isRunning = false;
+        thread = new Thread(this, "Display");
+        thread.start();
+    }
+    
+    @Override
+    public void run() {
+        
+        init();
+        
+        while(isRunning) {
+            
+        }
+        
+    }
 
     public static void main(String[] args) {
+        
+        new Main().start();
 
     }
 }
