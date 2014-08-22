bf612af68bbfd8269ac931515e41e8c73c1f2bb2
robanator1617 <robbie.harding@hotmail.com>
2014-08-22 10:51:22 -0400

    finished temp converter!

--- src/net/hackforums/morningstretches/TemperatureConverter.java
+++ src/net/hackforums/morningstretches/TemperatureConverter.java
@@ -4,44 +4,33 @@ import java.util.Scanner;
 
 class TemperatureConverter {
   
-  private static void celcius() {
-    Scanner scanner = new Scanner();
-    System.out.println("What is the temperature in celcius?");
-    double input = scanner.nextDouble();
-    System.out.println(input + " celcius is " + (input*9/5+32) + " farenheight" );
-  }
-  
-  private static void farenheight() {
-    Scanner scanner = new Scanner();
-    System.out.println("What is the temperature in farenheight?");
-    double input = scanner.nextDouble();
-    System.out.println(input + " celcius is " + (input-32/5*9) + " farenheight" );
-  }
-  
-  public static void main(String[] args) {
-    
-    Scanner scanner = new Scanner();
-    
-    while(true) {
-        System.out.println("Would you like to convert: [1] celcius to farenheight, [2] farenheight to celcius");
-        int input = scanner.nextInt();
-        switch(input) {
-          case 1:
-            celcius();
-            break;
-          case 2:
-            farenheight();
-            break;
-        }
-        System.out.println("Would you like to quit?(y/n)");
-        String banana = scanner.nextLine();
-        if(banana.equalsIgnoreCase("y")) {
-          break;
-        } else {
-          continue;
-        }
-    }
-    
-  }
-  
+	private static void celcius() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What is the temperature in celcius?");
+		double input = scanner.nextDouble();
+		System.out.println(input + " celcius is " + (input*9/5+32) + " farenheight" );
+	}
+	
+	private static void farenheight() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What is the temperature in farenheight?");
+		double input = scanner.nextDouble();
+		System.out.println(input + " celcius is " + (input-32/5*9) + " farenheight" );
+		System.out.println("Would you like to quit?(y/n)");
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+				System.out.println("Would you like to do: [1] convert celcius to farenheight, [2] convert farenheight to celcius, [3] quit");
+				int input = scanner.nextInt();
+				if(input == 1) {
+					celcius();
+				} else if(input == 2) {
+					farenheight();
+				} else if(input == 3) {
+					System.exit(0);
+				}
+			}
+	}
 }
