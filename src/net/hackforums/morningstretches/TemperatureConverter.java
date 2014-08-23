3dcf18be16a49a76e4ed77d101da7209225ad965
M1kep <M1kep.my.mail@gmail.com>
2014-08-23 16:22:38 -0700

    Neatening the code and fixing typos, general clean up

--- src/net/hackforums/morningstretches/TemperatureConverter.java
+++ src/net/hackforums/morningstretches/TemperatureConverter.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 
 class TemperatureConverter {
   
-	private static void celcius() {
+	private static void celsius() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("What is the temperature in celcius?");
+		System.out.println("What is the temperature in celsius?");
 		double input = scanner.nextDouble();
-		System.out.println(input + " celcius is " + (input*9/5+32) + " farenheight" );
+		System.out.println(input + " celsius is " + (input*9/5+32) + " fahrenheit" );
 	}
 	
-	private static void farenheight() {
+	private static void fahrenheit() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("What is the temperature in farenheight?");
+		System.out.println("What is the temperature in fahrenheit?");
 		double input = scanner.nextDouble();
-		System.out.println(input + " celcius is " + (input-32/5*9) + " farenheight" );
+		System.out.println(input + " celsius is " + (input-32/5*9) + " fahrenheit" );
 		System.out.println("Would you like to quit?(y/n)");
 	}
 	
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner consoleScanner = new Scanner(System.in);
 		while(true) {
-				System.out.println("Would you like to do: [1] convert celcius to farenheight, [2] convert farenheight to celcius, [3] quit");
-				int input = scanner.nextInt();
-				if(input == 1) {
-					celcius();
-				} else if(input == 2) {
-					farenheight();
-				} else if(input == 3) {
+				System.out.print("Would you like to do: [1] convert celsius to fahrenheit, [2] convert fahrenheit to celsius, [3] quit: ");
+				int userInput = consoleScanner.nextInt();
+				if(userInput == 1) {
+					celsius();
+				} else if(userInput == 2) {
+					fahrenheit();
+				} else if(userInput == 3) {
 					System.exit(0);
 				}
 			}
