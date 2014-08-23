9a8bdecc67943d82720016a082936fea9582db7a
M1kep <M1kep.my.mail@gmail.com>
2014-08-23 16:00:26 -0700

    attempting to make the temperature converter safe.

--- src/net/hackforums/morningstretches/TemperatureConverter.java
+++ src/net/hackforums/morningstretches/TemperatureConverter.java
@@ -1,19 +1,27 @@
 package net.hackforums.morningstretches;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class TemperatureConverter implements Runnable {
 
     private static void celsius(Scanner userInput) {
         System.out.print("Please enter the temperature in degrees celsius: ");
-        double input = userInput.nextDouble();
-        System.out.println(input + " celsius is " + (input * 9 / 5 + 32) + "degrees fahrenheit.");
+        double input = 0;
+        try {
+            input = userInput.nextDouble();
+            System.out.println(input + " celsius is " + (input * 9 / 5 + 32) + "degrees fahrenheit.");
+        } catch (InputMismatchException e) {
+        }
     }
 
     private static void fahrenheit(Scanner userInput) {
         System.out.print("Please enter the temperature in fahrenheit: ");
-        double input = userInput.nextDouble();
-        System.out.println(input + " celsius is " + (input - 32 / 5 * 9) + "degrees fahrenheit.");
+        try {
+            double input = userInput.nextDouble();
+            System.out.println(input + " celsius is " + (input - 32 / 5 * 9) + "degrees fahrenheit.");
+        } catch (InputMismatchException e) {
+        }
     }
 
 //    public static void main(String[] args) {
@@ -36,20 +44,18 @@ class TemperatureConverter implements Runnable {
         Scanner consoleScanner = new Scanner(System.in);
         while (true) {
             System.out.print("Would you like to do: [1] convert celsius to fahrenheit, [2] convert fahrenheit to celsius, or [3] quit: ");
-            int input = consoleScanner.nextInt();
-            if (input == 1) {
-                celsius(consoleScanner);
-            } else if (input == 2) {
-                fahrenheit(consoleScanner);
-            } else if (input == 3) {
-                System.exit(0);
-//                try {
-//                    System.out.println("Have a great day! Goodbye!");
-//                    this.wait(100);
-//                    System.exit(0);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
+            int input = 0;
+            try {
+                input = consoleScanner.nextInt();
+
+                if (input == 1) {
+                    celsius(consoleScanner);
+                } else if (input == 2) {
+                    fahrenheit(consoleScanner);
+                } else if (input == 3) {
+                    System.exit(0);
+                }
+            } catch (InputMismatchException e) {
             }
         }
     }
