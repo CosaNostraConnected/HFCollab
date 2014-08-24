cb43a371419ad5a6c2a14e05c04e1b3ecea3f7ed
M1kep <M1kep.my.mail@gmail.com>
2014-08-23 21:58:45 -0700

    Added Windows native files for LWJGL, and working on main game class(Mainly figuring things out)

--- src/net/hackforums/m1kep/gameattempt/Game.java
+++ src/net/hackforums/m1kep/gameattempt/Game.java
@@ -2,7 +2,6 @@ package net.hackforums.m1kep.gameattempt;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.GL11;
 public class Game {
 
     public static void main(String[] args) throws LWJGLException{
