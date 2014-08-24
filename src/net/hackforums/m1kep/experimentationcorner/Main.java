d66637092eda46d734c9bb4fe4024d23fa3ecc0c
M1kep <M1kep.my.mail@gmail.com>
2014-08-23 23:12:38 -0700

    Working on initial display.

--- src/net/hackforums/m1kep/experimentationcorner/Main.java
+++ src/net/hackforums/m1kep/experimentationcorner/Main.java
@@ -1,8 +1,17 @@
 package net.hackforums.m1kep.experimentationcorner;
 
+import org.lwjgl.BufferUtils;
+
+import java.nio.ByteBuffer;
+
 public class Main {
 
   public static void main(String[] args) {
+
+      ByteBuffer buffer = BufferUtils.createByteBuffer(4);
+
+     // buffer.put(a);
+
 
   }
 }
--- src/net/hackforums/m1kep/gameattempt/Game.java
+++ src/net/hackforums/m1kep/gameattempt/Game.java
@@ -1,17 +1,92 @@
 package net.hackforums.m1kep.gameattempt;
 
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import static org.lwjgl.opengl.GL11.*;
+
+
 public class Game {
 
-    public static void main(String[] args) throws LWJGLException{
+    public static final boolean VSYNC = true;
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+
+    public static final boolean FULLSCREEN = false;
+
+    protected boolean isRunning = false;
+
+
+    public static void main(String[] args) throws LWJGLException {
 
         new Game().start();
     }
 
-    public void start() throws LWJGLException{
-        Display.setTitle("Test");
-        Display.create();
+    public void start(){
+        try {
+            Display.setTitle("openGL learning!");
+            Display.setResizable(true);
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.setVSyncEnabled(VSYNC);
+            Display.setFullscreen(FULLSCREEN);
+
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+
+        create();
+
+        resize();
+
+        isRunning = true;
+
+        while (isRunning && !Display.isCloseRequested()) {
+            if (Display.wasResized()) {
+                resize();
+
+                render();
+
+                Display.update();
+                Display.sync(60);
+
+            }
+
+        }
+        dispose();
+        Display.destroy();
+
+    }
+
+    protected void create() {
+        glDisable(GL_DEPTH_TEST);
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        glClearColor(1f, 0f, 0f, 1f);
+
+    }
+
+    public void exit() {
+        isRunning = false;
+    }
+
+    protected void render() {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+    protected void resize() {
+        glViewport(0, 0, Display.getWidth(), Display.getHeight());
+
+    }
+
+    protected void dispose() {
 
     }
 }
